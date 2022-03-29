b2c6c6ce353293ac21c7fed7b35002da22a40ec1
Miguel Perez <miguellrvc@gmial.com>
2022-03-29 13:24:26 -0300

    cors configurados

--- src/main/java/com/porfolio/api/ApiApplication.java
+++ src/main/java/com/porfolio/api/ApiApplication.java
@@ -2,6 +2,9 @@ package com.porfolio.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +13,15 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
